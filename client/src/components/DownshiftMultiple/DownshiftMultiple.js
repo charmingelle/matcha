7d629b06805ff73d33bf51c6494531b7d76ea99d
charmingelle <revenkoaa89@gmail.com>
2018-10-30 18:25:04 +0200

    added storage to DownshiftMultiple component

--- client/src/components/DownshiftMultiple/DownshiftMultiple.js
+++ client/src/components/DownshiftMultiple/DownshiftMultiple.js
@@ -1,56 +1,99 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import deburr from 'lodash/deburr';
-import keycode from 'keycode';
-import Downshift from 'downshift';
-import { withStyles } from '@material-ui/core/styles';
-import TextField from '@material-ui/core/TextField';
-import Popper from '@material-ui/core/Popper';
-import Paper from '@material-ui/core/Paper';
-import MenuItem from '@material-ui/core/MenuItem';
-import Chip from '@material-ui/core/Chip';
+import React from "react";
+import PropTypes from "prop-types";
+import deburr from "lodash/deburr";
+import keycode from "keycode";
+import Downshift from "downshift";
+import { withStyles } from "@material-ui/core/styles";
+import TextField from "@material-ui/core/TextField";
+import Popper from "@material-ui/core/Popper";
+import Paper from "@material-ui/core/Paper";
+import MenuItem from "@material-ui/core/MenuItem";
+import Chip from "@material-ui/core/Chip";
 
-const suggestions = [
-  { label: 'reading' },
-  { label: 'yoga' },
-  { label: 'dancing' },
-  { label: 'video games' },
-  { label: 'diving' },
-];
+import { createStore } from "redux";
+
+let interests = [];
+
+const addInterests = (state = [], action) => {
+  if (action.type === 'ADD_INTEREST') {
+    return [
+      ...state,
+      action.interest
+    ]
+  }
+  return state;
+};
+
+let store = createStore(addInterests);
+
+store.subscribe(() => {
+  // console.log('subscribe', store.getState());
+  interests = [...interests, ...store.getState()];
+  // console.log('interests', interests);
+  console.log(interests);
+});
+
+// store.dispatch({'type': 'ADD_INTEREST', 'interest': {'label': 'running'}});
+// store.dispatch({'type': 'ADD_INTEREST', 'interest': {'label': 'singing'}});
+
+
+fetch("/interests", {
+  method: "POST",
+  credentials: "include"
+})
+  .then(response => response.json())
+  .then(interests => {
+    interests.forEach(interest => {
+      store.dispatch({'type': 'ADD_INTEREST', 'interest': interest});
+    });
+  });
+
+function enterPressHandler(event) {
+  if (event.key === 'Enter') {
+    store.dispatch({'type': 'ADD_INTEREST', 'interest': event.target.value});
+  }
+}
 
 function renderInput(inputProps) {
   const { InputProps, classes, ref, ...other } = inputProps;
 
   return (
     <TextField
-      InputProps={{
+    InputProps={{
         inputRef: ref,
         classes: {
           root: classes.inputRoot,
-          input: classes.inputInput,
+          input: classes.inputInput
         },
-        ...InputProps,
+        onKeyPress: enterPressHandler,
+        ...InputProps
       }}
       {...other}
     />
   );
 }
 
-function renderSuggestion({ suggestion, index, itemProps, highlightedIndex, selectedItem }) {
+function renderSuggestion({
+  suggestion,
+  index,
+  itemProps,
+  highlightedIndex,
+  selectedItem
+}) {
   const isHighlighted = highlightedIndex === index;
-  const isSelected = (selectedItem || '').indexOf(suggestion.label) > -1;
+  const isSelected = (selectedItem || "").indexOf(suggestion) > -1;
 
   return (
     <MenuItem
       {...itemProps}
-      key={suggestion.label}
+      key={suggestion}
       selected={isHighlighted}
       component="div"
       style={{
-        fontWeight: isSelected ? 500 : 400,
+        fontWeight: isSelected ? 500 : 400
       }}
     >
-      {suggestion.label}
+      {suggestion}
     </MenuItem>
   );
 }
@@ -59,19 +102,20 @@ renderSuggestion.propTypes = {
   index: PropTypes.number,
   itemProps: PropTypes.object,
   selectedItem: PropTypes.string,
-  suggestion: PropTypes.shape({ label: PropTypes.string }).isRequired,
+  suggestion: PropTypes.shape({ label: PropTypes.string }).isRequired
 };
 
-function getSuggestions(value) {
+function getInterests(value) {
   const inputValue = deburr(value.trim()).toLowerCase();
   const inputLength = inputValue.length;
   let count = 0;
 
   return inputLength === 0
     ? []
-    : suggestions.filter(suggestion => {
+    : store.getState().filter(suggestion => {
         const keep =
-          count < 5 && suggestion.label.slice(0, inputLength).toLowerCase() === inputValue;
+          count < 5 &&
+          suggestion.slice(0, inputLength).toLowerCase() === inputValue;
 
         if (keep) {
           count += 1;
@@ -83,15 +127,19 @@ function getSuggestions(value) {
 
 class DownshiftMultiple extends React.Component {
   state = {
-    inputValue: '',
-    selectedItem: [],
+    inputValue: "",
+    selectedItem: []
   };
 
   handleKeyDown = event => {
     const { inputValue, selectedItem } = this.state;
-    if (selectedItem.length && !inputValue.length && keycode(event) === 'backspace') {
+    if (
+      selectedItem.length &&
+      !inputValue.length &&
+      keycode(event) === "backspace"
+    ) {
       this.setState({
-        selectedItem: selectedItem.slice(0, selectedItem.length - 1),
+        selectedItem: selectedItem.slice(0, selectedItem.length - 1)
       });
     }
   };
@@ -108,8 +156,8 @@ class DownshiftMultiple extends React.Component {
     }
 
     this.setState({
-      inputValue: '',
-      selectedItem,
+      inputValue: "",
+      selectedItem
     });
   };
 
@@ -138,7 +186,7 @@ class DownshiftMultiple extends React.Component {
           isOpen,
           inputValue: inputValue2,
           selectedItem: selectedItem2,
-          highlightedIndex,
+          highlightedIndex
         }) => (
           <div className={classes.container}>
             {renderInput({
@@ -156,20 +204,20 @@ class DownshiftMultiple extends React.Component {
                 )),
                 onChange: this.handleInputChange,
                 onKeyDown: this.handleKeyDown,
-                placeholder: 'Select multiple interests',
+                placeholder: "Select multiple interests"
               }),
-              label: 'Interests',
+              label: "Interests"
             })}
             {isOpen ? (
               <Paper className={classes.paper} square>
-                {getSuggestions(inputValue2).map((suggestion, index) =>
+                {getInterests(inputValue2).map((suggestion, index) =>
                   renderSuggestion({
                     suggestion,
                     index,
-                    itemProps: getItemProps({ item: suggestion.label }),
+                    itemProps: getItemProps({ item: suggestion }),
                     highlightedIndex,
-                    selectedItem: selectedItem2,
-                  }),
+                    selectedItem: selectedItem2
+                  })
                 )}
               </Paper>
             ) : null}
@@ -181,40 +229,40 @@ class DownshiftMultiple extends React.Component {
 }
 
 DownshiftMultiple.propTypes = {
-  classes: PropTypes.object.isRequired,
+  classes: PropTypes.object.isRequired
 };
 
 const styles = theme => ({
   root: {
-    margin: '8px',
+    margin: "8px",
     flexGrow: 1,
-    height: 250,
+    height: 250
   },
   container: {
-    margin: '8px',
+    margin: "8px",
     flexGrow: 1,
-    position: 'relative',
+    position: "relative"
   },
   paper: {
-    position: 'absolute',
+    position: "absolute",
     zIndex: 1,
     marginTop: theme.spacing.unit,
     left: 0,
-    right: 0,
+    right: 0
   },
   chip: {
-    margin: `${theme.spacing.unit / 2}px ${theme.spacing.unit / 4}px`,
+    margin: `${theme.spacing.unit / 2}px ${theme.spacing.unit / 4}px`
   },
   inputRoot: {
-    flexWrap: 'wrap',
+    flexWrap: "wrap"
   },
   inputInput: {
-    width: 'auto',
-    flexGrow: 1,
+    width: "auto",
+    flexGrow: 1
   },
   divider: {
-    height: theme.spacing.unit * 2,
-  },
+    height: theme.spacing.unit * 2
+  }
 });
 
 let popperNode;
@@ -230,7 +278,7 @@ function IntegrationDownshift(props) {
 }
 
 IntegrationDownshift.propTypes = {
-  classes: PropTypes.object.isRequired,
+  classes: PropTypes.object.isRequired
 };
 
 export default withStyles(styles)(IntegrationDownshift);
--- server/server.js
+++ server/server.js
@@ -4,36 +4,36 @@ const port = 5000;
 const { Pool } = require('pg');
 const dotenv = require('dotenv');
 
-dotenv.config();
+// dotenv.config();
 
-const pool = new Pool({
-  connectionString: process.env.DATABASE_URL
-});
+// const pool = new Pool({
+//   connectionString: process.env.DATABASE_URL
+// });
 
-pool.on('connect', () => {
-  console.log('connected to the db');
-});
+// pool.on('connect', () => {
+//   console.log('connected to the db');
+// });
 
-const queryText =
-    `CREATE TABLE IF NOT EXISTS
-      reflections(
-        id UUID PRIMARY KEY,
-        success VARCHAR(128) NOT NULL,
-        low_point VARCHAR(128) NOT NULL,
-        take_away VARCHAR(128) NOT NULL,
-        created_date TIMESTAMP,
-        modified_date TIMESTAMP
-      )`;
+// const queryText =
+//     `CREATE TABLE IF NOT EXISTS
+//       reflections(
+//         id UUID PRIMARY KEY,
+//         success VARCHAR(128) NOT NULL,
+//         low_point VARCHAR(128) NOT NULL,
+//         take_away VARCHAR(128) NOT NULL,
+//         created_date TIMESTAMP,
+//         modified_date TIMESTAMP
+//       )`;
 
-pool.query(queryText)
-  .then((res) => {
-    console.log(res);
-    pool.end();
-  })
-  .catch((err) => {
-    console.log(err);
-    pool.end();
-  });
+// pool.query(queryText)
+//   .then((res) => {
+//     console.log(res);
+//     pool.end();
+//   })
+//   .catch((err) => {
+//     console.log(err);
+//     pool.end();
+//   });
 
 app.listen(port, () => console.log(`The server is running on port ${port}`));
 
@@ -45,4 +45,33 @@ app.get("/customers", (req, res) => {
   ];
 
   res.json(customers);
+});
+
+app.get("/customers", (req, res) => {
+  const customers = [
+    {id: 1, firstName: 'John', lastName: 'Doe'},
+    {id: 2, firstName: 'Mary', lastName: 'Adams'},
+    {id: 3, firstName: 'Richard', lastName: 'Smith'}
+  ];
+
+  res.json(customers);
+});
+
+app.post("/interests", (req, res) => {
+  // const interests = [
+  //   { label: 'reading' },
+  //   { label: 'yoga' },
+  //   { label: 'dancing' },
+  //   { label: 'video games' },
+  //   { label: 'diving' },
+  // ];
+  const interests = [
+    'reading',
+    'yoga',
+    'dancing',
+    'video games',
+    'diving',
+  ];
+
+  res.json(interests);
 });
