0fb66d4f8ce0c2f56663ad609d90a48775414cb4
charmingelle <revenkoaa89@gmail.com>
2018-11-18 20:30:09 +0200

    fixed sign in bug

--- server/server.js
+++ server/server.js
@@ -161,20 +161,24 @@ app.post('/getUsers', requireLogin, (req, res) => {
 });
 
 app.post('/signin', (req, res) => {
-  db.any('SELECT id, password FROM users WHERE login = ${login}', {
+  db.any('SELECT password FROM users WHERE login = ${login}', {
     login: req.body.login
   }).then(data => {
     if (data.length === 1) {
       bcrypt.compare(req.body.password, data[0].password).then(result => {
         if (result === true) {
           req.session.login = req.body.login;
-          res.status(200).send(JSON.stringify({ id: data[0].id }));
+          res.status(200).send();
         } else {
           res
             .status(500)
             .send(JSON.stringify({ result: 'Invalid login or password' }));
         }
       });
+    } else {
+      res
+        .status(500)
+        .send(JSON.stringify({ result: 'Invalid login or password' }));
     }
   });
 });
