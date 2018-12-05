7d1d368fbdf42fc6b829b250a9830158158d17b1
charmingelle <revenkoaa89@gmail.com>
2018-12-05 17:51:17 +0200

    moved input and send button back; added saving logs in Chat

--- client/src/components/Chat/Chat.js
+++ client/src/components/Chat/Chat.js
@@ -5,8 +5,6 @@ import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
 import ListItemText from '@material-ui/core/ListItemText';
 import Divider from '@material-ui/core/Divider';
-import Button from '@material-ui/core/Button';
-import Input from '@material-ui/core/Input';
 import { getChatLogins } from './../../api/api.js';
 import socketIOClient from 'socket.io-client';
 import Room from './Room/Room.js';
@@ -29,102 +27,51 @@ const styles = theme => ({
   },
   selectedUser: {
     backgroundColor: 'rgba(0, 0, 0, 0.08)'
-  },
-  marioChat: {
-    flexGrow: 1,
-    display: 'flex',
-    flexDirection: 'column',
-    width: '80%'
-  },
-  message: {
-    width: '100%',
-    padding: '14px'
-  },
-  send: {
-    padding: '18px 0',
-    width: '100%',
-    border: 'none',
-    outline: '1px solid #e9e9e9'
   }
 });
 
 class Chat extends React.Component {
   constructor() {
     super();
+    this.users = {};
     this.socket = socketIOClient('http://localhost:5000');
+    this.socket.emit('test');
   }
 
   componentDidMount = () => {
     this.props.changeTab(2);
     getChatLogins()
       .then(response => response.json())
-      .then(users =>
+      .then(users => {
+        users.forEach(
+          user =>
+            (this.users[user] = {
+              log: [],
+              draft: ''
+            })
+        );
         this.setState({
-          users,
-          selectedUser: users[0],
-          drafts: {},
-          message: ''
-        })
-      );
+          selectedUser: users[0]
+        });
+      });
   };
 
-  // saveDraft = (receiver, draft) => {
-  //   let newDrafts = this.state.drafts;
-
-  //   newDrafts[receiver] = draft;
-  //   this.setState({
-  //     drafts: newDrafts
-  //   });
-  //   console.log('this.state.drafts', this.state.drafts);
-  // };
-
-  changeHandler = event => {
-    this.socket.emit('typing', {
-      sender: this.props.sender,
-      receiver: this.state.selectedUser
-    });
-    if (event.target.value === '') {
-      this.socket.emit('stoppedTyping', {
-        sender: this.props.sender,
-        receiver: this.state.selectedUser
-      });
-    }
-    this.setState({
-      message: event.target.value
-    });
-  };
-
-  keyPressHandler = event => {
-    if (event.key === 'Enter') {
-      this.send();
-    }
-  };
-
-  send = () => {
-    if (this.state.message !== '') {
-      this.socket.emit('chat', {
-        sender: this.props.sender,
-        receiver: this.state.selectedUser,
-        message: this.state.message
-      });
-      this.setState({
-        message: ''
-      });
-    }
+  updateLog = (receiver, newLog) => {
+    this.users[receiver].log = newLog;
   };
 
   render = () => {
     if (!this.state) {
       return <span>Loading...</span>;
     }
-    const { users, selectedUser, message } = this.state;
+    const { selectedUser } = this.state;
     const { classes } = this.props;
 
     return (
       <BrowserRouter>
         <div className={classes.root}>
           <List component="nav" className={classes.users}>
-            {users.map((user, index) => (
+            {Object.keys(this.users).map((user, index) => (
               <Link className={classes.link} key={index} to={`/chat/${user}`}>
                 <ListItem
                   button
@@ -137,35 +84,19 @@ class Chat extends React.Component {
               </Link>
             ))}
           </List>
-          <div className={classes.marioChat}>
-              <Route
-                exact
-                path="/chat/:receiver"
-                component={({ match }) => (
-                  <Room
-                    socket={this.socket}
-                    sender={this.props.sender}
-                    receiver={match.params.receiver}
-                  />
-                )}
+          <Route
+            exact
+            path="/chat/:receiver"
+            component={({ match }) => (
+              <Room
+                socket={this.socket}
+                sender={this.props.sender}
+                receiver={match.params.receiver}
+                log={this.users[match.params.receiver].log}
+                updateLog={this.updateLog}
               />
-              <Input
-                type="text"
-                className={classes.message}
-                value={message}
-                placeholder="Say something..."
-                onChange={this.changeHandler}
-                onKeyPress={this.keyPressHandler}
-                disableUnderline={true}
-              />
-              <Button
-                className={classes.send}
-                onClick={this.send}
-                variant="outlined"
-              >
-                Send
-              </Button>
-          </div>
+            )}
+          />
         </div>
       </BrowserRouter>
     );
--- client/src/components/Chat/Room/Room.js
+++ client/src/components/Chat/Room/Room.js
@@ -1,9 +1,17 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import Input from '@material-ui/core/Input';
+import Button from '@material-ui/core/Button';
 import { withStyles } from '@material-ui/core/styles';
 import { getMessages } from './../../../api/api.js';
 
 const styles = theme => ({
+  marioChat: {
+    flexGrow: 1,
+    display: 'flex',
+    flexDirection: 'column',
+    width: '80%'
+  },
   chatWindow: {
     flexGrow: 1,
     overflow: 'auto',
@@ -21,6 +29,16 @@ const styles = theme => ({
     color: '#aaa',
     padding: '14px 0px',
     margin: '0 14px'
+  },
+  message: {
+    width: '100%',
+    padding: '14px'
+  },
+  send: {
+    padding: '18px 0',
+    width: '100%',
+    border: 'none',
+    outline: '1px solid #e9e9e9'
   }
 });
 
@@ -30,6 +48,7 @@ class Room extends React.Component {
     this.state = {
       sender: this.props.sender,
       receiver: this.props.receiver,
+      message: '',
       log: [],
       typing: false,
       isLoading: false,
@@ -39,52 +58,66 @@ class Room extends React.Component {
     this.socket = this.props.socket;
   }
 
+  addSocketEventListeners = () => {
+    this.socket.on('chat', data => {
+      let user = null;
+
+      if (this.state.sender === data.sender) {
+        user = data.receiver;
+      } else if (this.state.sender === data.receiver) {
+        user = data.sender;
+      }
+      if (user) {
+        let newLog = this.state.log;
+
+        newLog.unshift(data);
+        this.setState({
+          log: newLog,
+          typing: false
+        });
+      }
+    });
+    this.socket.on('typing', data => {
+      if (this.state.sender === data.receiver) {
+        this.setState({
+          typing: true
+        });
+      }
+    });
+    this.socket.on('stoppedTyping', data => {
+      if (this.state.sender === data.receiver) {
+        this.setState({
+          typing: false
+        });
+      }
+    });
+  };
+
   componentDidMount = () => {
-    getMessages(this.state.sender, this.state.receiver, this.state.lastloadedid)
-      .then(response => response.json())
-      .then(log => {
-        if (log.length !== 0) {
-          this.setState({ lastloadedid: log[log.length - 1].id });
-        }
-        this.setState({ log });
-      })
-      .then(() => {
-        this.socket.on('chat', data => {
-          let user = null;
-
-          if (this.state.sender === data.sender) {
-            user = data.receiver;
-          } else if (this.state.sender === data.receiver) {
-            user = data.sender;
-          }
-          if (user) {
-            let newLog = this.state.log;
-
-            newLog.unshift(data);
-            this.setState({
-              log: newLog,
-              typing: false
-            });
-          }
-        });
-        this.socket.on('typing', data => {
-          if (this.state.sender === data.receiver) {
-            this.setState({
-              typing: true
-            });
-          }
-        });
-        this.socket.on('stoppedTyping', data => {
-          if (this.state.sender === data.receiver) {
-            this.setState({
-              typing: false
-            });
-          }
-        });
+    this.addSocketEventListeners();
+    if (this.props.log.length !== 0) {
+      this.setState({
+        log: this.props.log,
+        lastloadedid: this.props.log[this.props.log.length - 1].id
       });
+    } else {
+      getMessages(
+        this.state.sender,
+        this.state.receiver,
+        this.state.lastloadedid
+      )
+        .then(response => response.json())
+        .then(log => {
+          if (log.length !== 0) {
+            this.setState({ lastloadedid: log[log.length - 1].id });
+          }
+          this.setState({ log });
+        });
+    }
   };
 
   componentWillUnmount = () => {
+    this.props.updateLog(this.state.receiver, this.state.log);
     this.socket.off('chat');
     this.socket.off('typing');
     this.socket.off('stoppedTyping');
@@ -130,30 +163,81 @@ class Room extends React.Component {
     }
   };
 
+  changeHandler = event => {
+    this.socket.emit('typing', {
+      sender: this.props.sender,
+      receiver: this.state.receiver
+    });
+    if (event.target.value === '') {
+      this.socket.emit('stoppedTyping', {
+        sender: this.props.sender,
+        receiver: this.state.receiver
+      });
+    }
+    this.setState({
+      message: event.target.value
+    });
+  };
+
+  keyPressHandler = event => {
+    if (event.key === 'Enter') {
+      this.send();
+    }
+  };
+
+  send = () => {
+    if (this.state.message !== '') {
+      this.socket.emit('chat', {
+        sender: this.props.sender,
+        receiver: this.state.receiver,
+        message: this.state.message
+      });
+      this.setState({
+        message: ''
+      });
+    }
+  };
+
   render = () => {
-    const { receiver, log, typing } = this.state;
+    const { receiver, message, log, typing } = this.state;
     const { classes } = this.props;
 
     return (
-      <div className={classes.chatWindow} onScroll={this.scrollHandler}>
-        <div>
-          {typing && (
-            <p className={classes.typingP}>
-              <em>{receiver} is typing a message...</em>
-            </p>
-          )}
+      <div className={classes.marioChat}>
+        <div className={classes.chatWindow} onScroll={this.scrollHandler}>
+          <div>
+            {typing && (
+              <p className={classes.typingP}>
+                <em>{receiver} is typing a message...</em>
+              </p>
+            )}
+          </div>
+          <div>
+            {log.map((record, index) => (
+              <p className={classes.outputP} key={index}>
+                <span>{`${new Date(
+                  parseInt(record.time)
+                ).toLocaleString()} `}</span>
+                <strong className={classes.outputString}>
+                  {record.sender}:
+                </strong>
+                {record.message}
+              </p>
+            ))}
+          </div>
         </div>
-        <div>
-          {log.map((record, index) => (
-            <p className={classes.outputP} key={index}>
-              <span>{`${new Date(
-                parseInt(record.time)
-              ).toLocaleString()} `}</span>
-              <strong className={classes.outputString}>{record.sender}:</strong>
-              {record.message}
-            </p>
-          ))}
-        </div>
+        <Input
+          type="text"
+          className={classes.message}
+          value={message}
+          placeholder="Say something..."
+          onChange={this.changeHandler}
+          onKeyPress={this.keyPressHandler}
+          disableUnderline={true}
+        />
+        <Button className={classes.send} onClick={this.send} variant="outlined">
+          Send
+        </Button>
       </div>
     );
   };
