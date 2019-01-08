6533b1fedd0261eee09f21574085aae1d9a41358
charmingelle <revenkoaa89@gmail.com>
2019-01-08 22:14:22 +0200

    added sticky session to websocket chat

--- server/chat.js
+++ server/chat.js
@@ -1,13 +1,25 @@
 module.exports = (app, db) => {
-  const io = require('socket.io')(app.listen(5000));
   const chatUsers = {};
 
-  io.use((socket, next) => {
-    chatUsers[socket.request._query.login] = socket.id;
-    next();
+  const sticky = require('socketio-sticky-session');
+
+  const options = {
+    proxy: true, //activate layer 4 patching
+    header: 'x-forwarded-for', //provide here your header containing the users ip
+    num: 2, //count of processes to create, defaults to maximum if omitted
+    sync: {
+      isSynced: true, //activate synchronization
+      event: 'mySyncEventCall' //name of the event you're going to call
+    }
+  };
+
+  const io = sticky(options, () => {
+    return require('socket.io')(app.listen(5000));
   });
 
   io.on('connection', socket => {
+    chatUsers[socket.request._query.login] = socket.id;
+
     socket.on('chat', data => {
       db.one(
         'INSERT INTO messages (sender, receiver, message, time) VALUES(${sender}, ${receiver}, ${message}, ${time}) RETURNING id, time',
@@ -77,4 +89,85 @@ module.exports = (app, db) => {
       });
     });
   });
+
+  // Without sticky session
+  
+  // const io = require('socket.io')(app.listen(5000));
+  // const chatUsers = {};
+
+  // io.use((socket, next) => {
+  //   chatUsers[socket.request._query.login] = socket.id;
+  //   next();
+  // });
+
+  // io.on('connection', socket => {
+  //   socket.on('chat', data => {
+  //     db.one(
+  //       'INSERT INTO messages (sender, receiver, message, time) VALUES(${sender}, ${receiver}, ${message}, ${time}) RETURNING id, time',
+  //       {
+  //         sender: data.sender,
+  //         receiver: data.receiver,
+  //         message: data.message,
+  //         time: Date.now()
+  //       }
+  //     ).then(result => {
+  //       io.to(chatUsers[data.sender]).emit('chat', {
+  //         id: result.id,
+  //         sender: data.sender,
+  //         receiver: data.receiver,
+  //         message: data.message,
+  //         time: result.time
+  //       });
+  //       io.to(chatUsers[data.receiver]).emit('chat', {
+  //         id: result.id,
+  //         sender: data.sender,
+  //         receiver: data.receiver,
+  //         message: data.message,
+  //         time: result.time
+  //       });
+  //     });
+  //   });
+
+  //   socket.on('typing', data =>
+  //     io.to(chatUsers[data.receiver]).emit('typing', data)
+  //   );
+
+  //   socket.on('stoppedTyping', data =>
+  //     io.to(chatUsers[data.receiver]).emit('stoppedTyping', data)
+  //   );
+
+  //   socket.on('like', data => {
+  //     db.any(
+  //       'SELECT * FROM likes WHERE liker = ${liker} AND likee = ${likee}',
+  //       {
+  //         liker: data.receiver,
+  //         likee: data.sender
+  //       }
+  //     ).then(result => {
+  //       if (result.length === 0) {
+  //         io.to(chatUsers[data.receiver]).emit('like', data);
+  //       } else {
+  //         io.to(chatUsers[data.receiver]).emit('likeBack', data);
+  //       }
+  //     });
+  //   });
+
+  //   socket.on('check', data =>
+  //     io.to(chatUsers[data.receiver]).emit('check', data)
+  //   );
+
+  //   socket.on('unlike', data => {
+  //     db.any(
+  //       'SELECT * FROM likes WHERE liker = ${liker} AND likee = ${likee}',
+  //       {
+  //         liker: data.receiver,
+  //         likee: data.sender
+  //       }
+  //     ).then(result => {
+  //       if (result.length !== 0) {
+  //         io.to(chatUsers[data.receiver]).emit('unlike', data);
+  //       }
+  //     });
+  //   });
+  // });
 };
