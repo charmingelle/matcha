9e915a5638d74b7f00ebf08ddbdf1cdd0660a211
charmingelle <revenkoaa89@gmail.com>
2019-01-09 00:49:27 +0200

    added built React app

---
+++ client/build/precache-manifest.2140f050825db8846b6204318eb621e4.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "04d3d843a75aa9638ff3c35b9f482fd3",
+    "url": "/static/media/7dc4faab62cbb0f25fe9571c488a8c2c706b0ba91546464545019.04d3d843.png"
+  },
+  {
+    "revision": "d96ede3e1d9c3fc436c7",
+    "url": "/static/js/main.d96ede3e.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "cd3383234a02a44c38cb5b89f6c00551",
+    "url": "/static/media/0c39a950b0eb0d0cf1659dadbe96bf554b63cbeb1546464237056.cd338323.png"
+  },
+  {
+    "revision": "fdc2da58018de15791f6af58e24f28e3",
+    "url": "/static/media/16fad309750dbe9966d5d19b07f688e320781c58.fdc2da58.png"
+  },
+  {
+    "revision": "f01d62892eae81132f266335e8417864",
+    "url": "/static/media/77a987bdfd500aad49eb1b4279ab1c2589ec1f2d1546962870237.f01d6289.png"
+  },
+  {
+    "revision": "b2400086a6b6e57cc7f6d96a842524e4",
+    "url": "/static/media/da706baf2ec84d9b7038cfc7faa7109d0b3ee038.b2400086.png"
+  },
+  {
+    "revision": "6b20cb7bb3e61977fd1708e7a8aecaeb",
+    "url": "/static/media/8c0e87e081e9145ac4a8661144a265fa31e4743a1546463825180.6b20cb7b.png"
+  },
+  {
+    "revision": "cd43fcfe2a18a556643ef3777a96c8dd",
+    "url": "/static/media/avatar.cd43fcfe.png"
+  },
+  {
+    "revision": "ca2c4f40546a570b7dc0b4b540d9ed1e",
+    "url": "/static/media/d5e463812c37b7337b2410c9b650326e066787191546464475276.ca2c4f40.png"
+  },
+  {
+    "revision": "9b0b963aa327b1860b5e",
+    "url": "/static/js/1.9b0b963a.chunk.js"
+  },
+  {
+    "revision": "b8ee5bf34271c4abe85e0a60a50cd2d3",
+    "url": "/index.html"
+  }
+];
